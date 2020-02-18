1bf9a919f4a464657ea4d8aa43bec1de96c0296a
idoheinemann <idohaineman@gmail.com>
2020-02-18 12:17:15 +0200

    now gets match itself and not event

--- TBA_API_port.py
+++ TBA_API_port.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_game(event_key):
-    return (requests.get(f"https://www.thebluealliance.com/api/v3/event/{event_key}/matches",
+    return (requests.get(f"https://www.thebluealliance.com/api/v3/match/{event_key}",
                          headers={
                              "X-TBA-Auth-Key": "nsydEycbcbK5YX4RK2eV9uoOBiFpkcivKdYlfFF0my3M6E9AvAqyB5ByrrQlYTjG"}))
 
@@ -14,22 +14,17 @@ def get_team_by_key(team_key):
 
 def get_teams_in_game(event_key):
     data = get_game(event_key).json()
-    matches = {}
-    for i in range(len(data)):
-        red_teams = []
-        blue_teams = []
-        for team in data[i]["alliances"]["red"]["team_keys"]:
-            red_teams.append(get_team_by_key(team)["nickname"])
-        for team in data[i]["alliances"]["blue"]["team_keys"]:
-            blue_teams.append(get_team_by_key(team)["nickname"])
-        if i > 6:
-            break
-        matches[f"match{i}"] = {"red" : red_teams, "blue" : blue_teams}
-    return matches
+    red_teams = []
+    blue_teams = []
+    for team in data["alliances"]["red"]["team_keys"]:
+        red_teams.append(get_team_by_key(team)["nickname"])
+    for team in data["alliances"]["blue"]["team_keys"]:
+        blue_teams.append(get_team_by_key(team)["nickname"])
+    return [red_teams, blue_teams]
 
 
 def main():
-    print(get_teams_in_game('2020week0'))
+    print(get_teams_in_game('2020week0_qm1'))
 
 
 if __name__ == '__main__':
